6c27782c56b6ab01f0b8d1cc0c3e35a562ed0764
Martina Cantaro <6120897+martinacantaro@users.noreply.github.com>
2021-10-09 02:46:14 -0300

    WIP Change log: - Upload new PyMC logo - Updated conf.py to pymc instead of pymc3 - Start configuring and populating index page - Start configuring and populating learning page - Start configuring and populating api page - Updated API to follow the docs/Architecture.png layout - Start configuring and populating learning page - Fix typo in filename of "index legacy"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -212,7 +212,7 @@ html_sidebars = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "../pymc_logo.jpg"
+html_logo = "../PyMC.jpg"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
